b5818941c053062b10321e6700808184eed5768e
praked <pranavkedia11@gmail.com>
2018-11-27 10:59:24 +0530

    update

--- Assignments/Team2/client/Encode.s
+++ Assignments/Team2/client/Encode.s
@@ -1,41 +1,77 @@
-  AREA appcode, CODE, READONLY
-		export __main
-		
+    AREA	Hamming, CODE, READONLY
+	EXPORT	__main
 		ENTRY
-__main  function
-		LDR R3,=0X20000011;
-		LDR R2,=0X20002001;
-		MOV R4,#10; MAX X
-		MOV R5,#5;  MAX Y
-		MOV R6,#8; ASSUMING THE ALL NUMBERS TO BE 1 BYTE IN SIZE
-		MUL R4,R4,R5;
-		MUL R4,R4,R6; R7 HAS TOTAL NO. OF BITS
-		MOV R7,#0; ITERATOR
-		MOV R8,#0;
-ENCODE 	CMP R7, R5 ;
-		BEQ STOP
-		LDRB R1,[R3,R7]
-		STRB R1,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		LDRB R5,[R3,R7]
-		STRB R5,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		LDRB R6,[R3,R7]
-		STRB R6,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		EOR R9,R1,R5;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		EOR R9,R6,R5;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		EOR R9,R1,R6;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		B    ENCODE        
-STOP    B STOP ; stop program
-     ENDFUNC
-	 END		 
+		
+__main FUNCTION
+
+start
+	; Load a test value into R1
+	
+	LDR	R1, =0xAC
+
+	; Begin by expanding the 8-bit value to 12-bits, inserting
+	; zeros in the positions for the four check bits (bit 0, bit 1, bit 3
+	; and bit 7).
+	
+	AND	R2, R1, #0x1		; Clear all bits apart from d0
+	MOV	R0, R2, LSL #2		; Align data bit d0
+	
+	AND	R2, R1, #0xE		; Clear all bits apart from d1, d2, & d3
+	ORR	R0, R0, R2, LSL #3	; Align data bits d1, d2 & d3 and combine with d0
+	
+	AND	R2, R1, #0xF0		; Clear all bits apart from d3-d7
+	ORR	R0, R0, R2, LSL #4	; Align data bits d4-d7 and combine with d0-d3
+	
+	; We now have a 12-bit value in R0 with empty (0) check bits in
+	; the correct positions
+	
+
+	; Generate check bit c0
+	
+	EOR	R2, R0, R0, LSR #2	; Generate c0 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #4	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x1		; Clear all but check bit c0
+	ORR	R0, R0, R2		; Combine check bit c0 with result
+	
+	; Generate check bit c1
+	
+	EOR	R2, R0, R0, LSR #1	; Generate c1 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #4	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x2		; Clear all but check bit c1
+	ORR	R0, R0, R2		; Combine check bit c1 with result
+	
+	; Generate check bit c2
+	
+	EOR	R2, R0, R0, LSR #1	; Generate c2 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #2	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x8		; Clear all but check bit c2
+	ORR	R0, R0, R2		; Combine check bit c2 with result	
+	
+	; Generate check bit c3
+	
+	EOR	R2, R0, R0, LSR #1	; Generate c3 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #2	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #4	; ... final iteration
+	
+	AND	R2, R2, #0x80		; Clear all but check bit c3
+	ORR	R0, R0, R2		; Combine check bit c3 with result
+	MOV R3, R0			
+	LSR R3, #2
+	AND R4, R0, #0X0F00 ;extract [11:8] bits
+	AND R5, R0, #0X0070 ;;extract [6:4] bits
+	LSL R5,R5,#1
+	AND R6, R3, #0X0001 ;extract first bit
+	LSL R6,R6, #4
+	ORR R4,R4,R5 
+	ORR R4,R4,R6
+	LSR R4,R4,#4;R4 Contains the final result
+stop B stop
+	ENDFUNC
+	END
+	
--- Assignments/Team2/server/Decode.s
+++ Assignments/Team2/server/Decode.s
@@ -1,41 +1,90 @@
-  AREA appcode, CODE, READONLY
-		export __main
-		
-		ENTRY
-__main  function
-		LDR R3,=0X20000011;
-		LDR R2,=0X20002001;
-		MOV R4,#10; MAX X
-		MOV R5,#5;  MAX Y
-		MOV R6,#8; ASSUMING THE ALL NUMBERS TO BE 1 BYTE IN SIZE
-		MUL R4,R4,R5;
-		MUL R4,R4,R6; R7 HAS TOTAL NO. OF BITS
-		MOV R7,#0; ITERATOR
-		MOV R8,#0;
-ENCODE 	CMP R7, R5 ;
-		BEQ STOP
-		LDRB R1,[R3,R7]
-		LDRB R5,[R3,R7]
-		LDRB R6,[R3,R7]
-		EOR R9,R1,R5;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		EOR R9,R6,R5;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		STRB R5,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		EOR R9,R1,R6;
-		STRB R9,[R2,R8]
-		ADD  R8,R8,#1
-		STRB R6,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		STRB R1,[R2,R8]
-		ADD  R7,R7,#1
-		ADD  R8,R8,#1
-		B    ENCODE        
-STOP    B STOP ; stop program
-     ENDFUNC
-	 END		 
+    AREA appcode,CODE,READONLY;
+	EXPORT __main
+      ENTRY
+__main FUNCTION	  
+ 	;Clear bits c0, c1, c3, c7
+	LDR R3, =0XFFFFFF74
+	AND R3, R0, R3
+
+
+
+	; Generate check bit c0
+	
+	EOR	R2, R3, R3, LSR #2	; Generate c0 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #4	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x1		; Clear all but check bit c0
+	ORR	R3, R3, R2		    ; Combine check bit c0 with result
+	
+	; Generate check bit c1
+	
+	EOR	R2, R3, R3, LSR #1	; Generate c1 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #4	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x2		; Clear all but check bit c1
+	ORR	R3, R3, R2		; Combine check bit c1 with result
+	
+	; Generate check bit c2
+	
+	EOR	R2, R3, R3, LSR #1	; Generate c2 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #2	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #8	; ... final iteration
+	
+	AND	R2, R2, #0x8		; Clear all but check bit c2
+	ORR	R3, R3, R2		; Combine check bit c2 with result	
+	
+	; Generate check bit c3
+	
+	EOR	R2, R3, R3, LSR #1	; Generate c3 parity bit using parity tree
+	EOR	R2, R2, R2, LSR #2	; ... second iteration ...
+	EOR	R2, R2, R2, LSR #4	; ... final iteration
+	
+	AND	R2, R2, #0x80		; Clear all but check bit c3
+	ORR	R3, R3, R2		; Combine check bit c3 with result
+
+
+	
+	;Compare the original value (with error) and the recalculated value using exclusive-OR
+	EOR R1, R0, R3
+
+
+	;Isolate the results of the EOR operatation to result in a 4-bit calculation
+
+	;Clearing all bits apart from c7 and shifting bit 4 positions right
+	LDR R4, =0X80
+	AND R4, R4, R1
+	MOV R4, R4, LSR #4
+
+	;Clearing all bits apart from c3 and shifting the 3rd bit 1 position right
+	LDR R5, =0X8
+	AND R5, R5, R1
+	MOV R5, R5, LSR #1
+
+	;Clearing all bits apart from c0 and c1  
+	LDR R6, =0X3
+	AND R6, R6, R1
+
+
+	;Adding the 4 registers together 
+	ADD R1, R4, R5
+	ADD R1, R1, R6 
+
+	;Subtracting 1 from R1 to determine the bit position of the error
+	SUB R1, R1, #1
+
+	;Store tmp register with binary 1. Then moves the 1, 8 bit positions left.  We use '8' because R1 contains 8 bits
+	LDR R7, =0X1
+	MOV R7, R7, LSL R1
+
+	;Flips the bit in bit 8 of R0
+	EOR R0, R0, R7
+
+	;Result =0x00000A6B
+
+stop	B	stop
+    ENDFUNC
+	END
+
+	END	
